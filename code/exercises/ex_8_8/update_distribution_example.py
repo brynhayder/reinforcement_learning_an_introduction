bc8d40677768f3b04491f12a2e2942b53d5f51c1
Bryn Elesedy <4146799+brynhayder@users.noreply.github.com>
2018-07-15 17:33:12 +0100

    chapter 8

--- code/exercises/ex_8_8/update_distribution_example.py
+++ code/exercises/ex_8_8/update_distribution_example.py
@@ -13,30 +13,44 @@ import numpy as np
 
 from generic import policies
 
+"""
+refactoring (model has too many responsibilities
+make an agent class and inject the model?
+
+
+
+"""
+
 
 class Model:
-    def __init__(self, n_states, branching_factor, random_state=None):
+    def __init__(self, n_states, n_actions_per_state, branching_factor, random_state=None):
         self.n_states = n_states
+        self.n_actions_per_state = n_actions_per_state
         self.branching_factor = branching_factor
         self.random_state = random_state or np.random.RandomState(seed=0)
 
         self.states = np.arange(n_states)
-        self.actions = np.arange(2)
+        self.actions = np.arange(n_actions_per_state)
         self.start_state = 0
         self.terminal_state = n_states - 1
         self.p_terminate = 0.1
 
-        self.non_terminal_states = [s for s in self.states if s != self.terminal_state]
+        self.non_terminal_states = np.array([s for s in self.states if s != self.terminal_state])
         self.current_state = self.start_state
 
         self._successor_table = dict()
         for a in self.actions:
             self._successor_table[self.terminal_state, a] = []
 
-        self._expected_reward_table = {
-            tup: self.random_state.normal(loc=0., scale=1.)
-            for tup in itertools.product(self.non_terminal_states, self.actions)
-        }
+        self._expected_reward_table = self.random_state.normal(
+                loc=0.,
+                scale=1.,
+                size=(n_states - 1, n_actions_per_state)
+        )
+        # self._expected_reward_table = {
+        #     tup: self.random_state.normal(loc=0., scale=1.)
+        #     for tup in itertools.product(self.non_terminal_states, self.actions)
+        # }
 
     def reset(self):
         self.current_state = self.start_state
@@ -59,7 +73,7 @@ class Model:
     def expected_reward(self, state, action):
         return self._expected_reward_table[state, action]
 
-    def initial_action_values(self, v=0.):
+    def initial_action_values(self, v):
         avs = {
             s: {a: v for a in self.actions} for s in self.states
         }
@@ -74,28 +88,38 @@ class Model:
         )
 
 
-def run_task(n_states, branching_factor, n_iters, n_updates_per_task, gamma, tolerance):
-    models = [
-        Model(
-                n_states=n_states,
-                branching_factor=branching_factor,
-                random_state=np.random.RandomState(seed=seed)
+class UniformUpdateAgent:
+    def __init__(self, model, gamma):
+
+
+
+def run_task(n_states, n_actions_per_state, branching_factor, n_iters,
+             n_updates_per_task, gamma, tolerance, policy_eval_iter):
+    args = [
+        (
+            Model(
+                    n_states=n_states,
+                    branching_factor=branching_factor,
+                    n_actions_per_state=n_actions_per_state,
+                    random_state=np.random.RandomState(seed=seed)
+            ),
+            n_updates_per_task,
+            gamma,
+            tolerance,
+            policy_eval_iter
         )
         for seed in range(n_iters)
     ]
     with ProcessPoolExecutor() as executor:
         values = executor.map(
-                starting_state_value_estimates,
-                models,
-                [n_updates_per_task for _ in models],
-                [gamma for _ in models],
-                [tolerance for _ in models]
+                uniform_starting_state_value_estimates,
+                *zip(*args)
         )
     return np.column_stack(values)
 
 
-def starting_state_value_estimates(model: "Model", n_updates: "int", gamma: "float",
-                                   tolerance: "float", policy_eval_iter: "int"=500):
+def uniform_starting_state_value_estimates(model: "Model", n_updates: "int", gamma: "float",
+                                           tolerance: "float", policy_eval_iter: "int"):
     av = model.initial_action_values(0.)
     starting_state_values = [0.]
     i = 1
@@ -131,7 +155,7 @@ def policy_evaluation(policy, model, tolerance, gamma, max_updates=10 ** 5):
             values[s] = (
                     model.expected_reward(s, a)
                     + gamma * ((1 - model.p_terminate) / model.branching_factor)
-                            * sum(values[ns] for ns in model.successors(s, a))
+                    * sum(values[ns] for ns in model.successors(s, a))
             )
             i += 1
             if i == max_updates:
@@ -143,23 +167,36 @@ def policy_evaluation(policy, model, tolerance, gamma, max_updates=10 ** 5):
 
 
 if __name__ == "__main__":
-    import matplotlib; matplotlib.use("TkAgg")
+    import matplotlib;
+
+    matplotlib.use("TkAgg")
     import matplotlib.pyplot as plt
+
+    import plotting
+    import constants as c
+
+    policy_eval_iter = 500
+    n_updates_per_task = 2 * 10 ** 3
 
     # uniform example
 
     output = run_task(
             n_states=1000,
+            n_actions_per_state=2,
             branching_factor=1,
             n_iters=200,
-            n_updates_per_task=2 * 10**4,
+            n_updates_per_task=n_updates_per_task,
             tolerance=0.01,
-            gamma=0.9
+            gamma=0.9,
+            policy_eval_iter=policy_eval_iter
     )
 
-    plt.plot(output, alpha=0.25)
-    plt.plot(np.mean(output, axis=1), color='k', lw=3)
-    plt.grid(alpha=0.1)
+    with plt.rc_context(plotting.rc()):
+        fig, ax = plt.subplots(1)
 
-    print('ready')
-    plt.show()
+        x = np.arange(output.shape[0]) * policy_eval_iter
+        ax.plot(x, np.mean(output, axis=1), color='k', lw=3)
+        ax.grid(alpha=0.1)
+
+        print('ready')
+        plt.show()
